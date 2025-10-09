68fc8a2b658beaf60b49d358681f7816a3c01f4d
maloe228 <149201142+maloe228@users.noreply.github.com>
2025-10-09 21:28:04 +0300

    Create 5.cpp

---
+++ first_semester/laboratory/1/5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    std::cout << "x y z | A B" << "\n";
+    std::cout << "-------------" << "\n";
+    for (int x = 0; x <= 1; ++x) {
+        for (int y = 0; y <= 1; ++y) {
+            for (int z = 0; z <= 1; ++z) {
+                bool A = ((x || y) && z || !(x || y) && !z) && (x && y && z);
+                bool B = !(x && y && z);
+                std::cout << x << " " << y << " " << z << " | " << A << " " << B << "\n";
+            }
+        }
+    }
+    return 0;
+}
